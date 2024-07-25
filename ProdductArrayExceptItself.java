0db9a7d27965207700ed385103cdee58328c6b0d
Chaitanya Arava <95595265+aravachaitanya@users.noreply.github.com>
2024-07-25 13:37:19 -0500

    Create ProdductArrayExceptItself.java

---
+++ ProdductArrayExceptItself.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int leftCount = 1;
+        for(int i=0; i< nums.length; i++) {
+            result[i] = leftCount;
+            leftCount *= nums[i];
+        }
+
+        int rightCount = 1;
+        for(int i=nums.length-1; i>=0; i--) {
+            result[i] *= rightCount;
+            rightCount *= nums[i];
+        }
+        return result;
+    }
+}
