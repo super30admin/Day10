e56f4eb7d5cb38a5e26076dbfa53896ca2824cb0
Preethi Arjun Ganesh <arjunasganesh.preethi@gmail.com>
2024-08-06 11:54:56 -0700

    Array-1 Completed

---
+++ SpiralMatrix.java
@@ -0,0 +1,45 @@
+//Time Complexity: O(m*n)
+//Space Complexity: O(m*n)
+import java.util.ArrayList;
+import java.util.List;
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer>  result = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = n-1;
+        int top = 0;
+        int bottom = m-1;
+        while(left<=right && top<=bottom){
+            for(int j = left;j<=right; j++){
+                result.add(matrix[top][j]);
+            }
+            top++;
+            if(left<=right && top<=bottom){
+                for(int i = top ; i<= bottom; i++){
+                    result.add(matrix[i][right]);
+                }
+                right--;
+            }
+            if(left<=right && top<=bottom){
+                for(int j = right; j>=left; j--){
+                    result.add(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+
+            if(left<=right && top<=bottom){
+                for(int i = bottom; i>=top;i--){
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+
+
+        }
+        return result;
+
+    }
+}
