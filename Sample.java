8fe940083eb54d73f7cbb11db3d08a0c91251286
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-10 13:18:32 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -78,3 +78,58 @@ class Solution {
         return result;
     }
 }
+
+
+
+// Diagnal traversal
+// TIME COMPLEXITY:O(m*n)
+//SPACE COMPLEXITY:O(1)
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        int size = m*n;
+        int[] result = new int[size];
+        int dir=1;
+        int index=0;
+        int row=0;
+        int col=0;
+        while (index < size) {
+            result[index] = mat[row][col];
+            index++;
+            if(dir == 1) {
+                if (col == n-1) {
+                    dir = -1;
+                    row++;
+                }
+                else if(row == 0) {
+                    dir = -1;
+                    col++;
+                }
+                else {
+                    row--;
+                    col++;
+                }
+
+            }
+            else if (dir == -1) {
+                if (row == m-1) {
+                    col++;
+                    dir = 1;
+                }
+                else if (col == 0)
+                {
+                    row++;
+                    dir =1;
+                }
+                else {
+                    col--;
+                    row++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
