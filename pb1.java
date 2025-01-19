c0fa75b0a17f6a39412c8b33fa52c9f78e9ff6d9
shivangiS04 <shivangimasterblaster@gmail.com>
2025-01-20 00:41:41 +0530

    git commit

---
+++ pb1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Problem1 {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+        // Step 1: Calculate prefix product
+        output[0] = 1;
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Calculate suffix product and update the result
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
