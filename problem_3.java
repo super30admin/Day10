f0e282dee69e20297545e1627ae777d31b79bf2f
Vedika Vishwanath Painjane <vp2312@rit.edu>
2025-02-04 13:03:07 -0500

    Completed Array–1

---
+++ problem_3.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+class problem_3 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> result = new ArrayList<>();
+        int top = 0; int bottom=m-1;
+        int left=0; int right=n-1;
+        while(top<=bottom&&left<=right){
+            //top
+            for (int j = left; j<=right;j++){
+                result.add(matrix[top][j]);
+
+            }
+            top++;
+            //right
+
+            
+            for (int i = top; i<=bottom;i++){
+            result.add(matrix[i][right]);
+
+            }
+            right--;
+            
+            
+
+            //bottom
+            if(top<=bottom){
+                for (int j = right; j>=left;j--){
+                result.add(matrix[bottom][j]);
+
+                }
+                bottom--;
+            }
+            
+
+            //left
+            if(left<=right){
+                for (int i = bottom; i>=top;i--){
+                result.add(matrix[i][left]);
+
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        problem_3 obj = new problem_3();
+        // test case 1
+        int [][] mat ={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(obj.spiralOrder(mat));
+    }
+}
+
+
