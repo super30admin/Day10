35b03f1fdb424b8b92d09c0409949609868439c2
PrajwalS Venkateshmurthy <psv.globallyunique@gmail.com>
2025-02-04 03:08:31 -0800

    Update 1

--- Problem1.py
+++ Problem1.py
@@ -2,7 +2,7 @@
 # 238. Product of Array Except Self
 
 # Time Complexity: O(n)
-# Space Complexity: O(n)
+# Space Complexity: O(1)
 # Did this code successfully run on Leetcode: Yes
 # Any problem you faced while coding this: No
 
